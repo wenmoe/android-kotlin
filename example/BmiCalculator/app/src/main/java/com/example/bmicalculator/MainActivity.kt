9b603cfdd873290e0f2dd2bad1420a4e6328d99b
eomnew <nacom1994@naver.com>
2021-01-04 21:32:47 +0900

    5장 - 완성

--- example/BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ example/BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.bmicalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceActivity
+import android.preference.PreferenceManager
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -10,5 +12,48 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadData()
+
+
+        resultButton.setOnClickListener {
+            /*
+            val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putExtra("weight", weightEditText.text.toString())
+            intent.putExtra("height", heightEditText.text.toString())
+
+            startActivity(intent)
+            */
+
+            saveData(heightEditText.text.toString().toInt(), weightEditText.text.toString().toInt())
+
+            //startActivity<ResultActivity>() // anko 사용
+            startActivity<ResultActivity>(
+                "weight" to weightEditText.text.toString(),
+                "height" to heightEditText.text.toString()
+            )
+
+        }
+    }
+
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+            .putInt("KEY_WEIGHT", weight)
+            .apply()
+    }
+
+    private fun loadData() {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val height = pref.getInt("KEY_HEIGHT", 0)
+        val weight = pref.getInt("KEY_WEIGHT", 0)
+
+        if (height != 0 && weight != 0) {
+            heightEditText.setText(height.toString())
+            weightEditText.setText(weight.toString())
+        }
     }
 }
